792088f1f3971943bd43d60788f2b493ca427f60
Alex Roth <alex@alexshiresroth.com>
2022-02-07 14:51:21 -0500

    add ef tracking and google analytics

--- src/pages/Thankyou.tsx
+++ src/pages/Thankyou.tsx
@@ -24,6 +24,74 @@ const Thankyou = () => {
     <ThemeContext.Provider value={Colors}>
       <Helmet>
         <title>Thank You</title>
+        <script
+          async
+          src="//loox.io/widget/loox.js?shop=luciana-rose-couture.myshopify.com"
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1003840432"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-1003840432')`,
+          }}
+        ></script>
+
+        <script src="https://www.poptrkr.com/scripts/sdk/everflow.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `EF.conversion({
+              offer_id: 75
+            })`,
+          }}
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1003840432"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` 
+      window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-1003840432');`,
+          }}
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-M66C9TR');`,
+          }}
+        ></script>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-306958873"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('js', new Date());
+
+            gtag('config', 'AW-306958873');`,
+          }}
+        ></script>
       </Helmet>
       <Main>
         <Globalstyle />
--- src/pages/otos/Oto1.tsx
+++ src/pages/otos/Oto1.tsx
@@ -23,6 +23,75 @@ const Oto1: FC = () => {
     <Main>
       <Helmet>
         <title>1CT Gold Studs</title>
+        <script
+          async
+          src="//loox.io/widget/loox.js?shop=luciana-rose-couture.myshopify.com"
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1003840432"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-1003840432')`,
+          }}
+        ></script>
+
+        <script src="https://www.poptrkr.com/scripts/sdk/everflow.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` EF.conversion({
+              offer_id: 75,
+              adv_event_id: 9
+            })`,
+          }}
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-1003840432"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` 
+      window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-1003840432');`,
+          }}
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-M66C9TR');`,
+          }}
+        ></script>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-306958873"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('js', new Date());
+
+            gtag('config', 'AW-306958873');`,
+          }}
+        ></script>
       </Helmet>
       <Globalstyle />
       <Alert />
