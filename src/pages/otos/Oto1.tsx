39a151333ea66139c6947644e049150b4c6da0ed
Alex <alexroth96@gmail.com>
2022-02-10 13:47:41 -0500

    add order revenue

--- src/pages/otos/Oto1.tsx
+++ src/pages/otos/Oto1.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import Alert from "../../components/alert/Alert";
 import OtoScreen from "../../components/otos/oto1/OtoScreen";
+import { useAppSelector } from "../../hooks/reduxHooks";
+import { selectOrderState } from "../../redux/reducers/order.reducer";
 import HelmetWrapper from "../layout/HelmetWrapper";
 const Main = styled.main``;
 
@@ -18,11 +20,16 @@ const Globalstyle = createGlobalStyle`
 }`;
 
 const Oto1: FC = () => {
+  const orderState = useAppSelector(selectOrderState);
+
+  const {
+    myOrder: { orderTotal },
+  } = orderState;
   return (
     <Main>
       <HelmetWrapper
         pageTitle="1CT Gold Studs"
-        efScript="EF.conversion({offer_id: 75});"
+        efScript={`EF.conversion({offer_id: 75, amount: ${orderTotal}});`}
       />
       <Globalstyle />
       <Alert />
