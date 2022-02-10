277754a75637a3cd0f21be8360e6fe2f8155a6c1
Alex <alexroth96@gmail.com>
2022-02-10 12:41:18 -0500

    test ef

--- src/pages/layout/HelmetWrapper.tsx
+++ src/pages/layout/HelmetWrapper.tsx
@@ -49,10 +49,41 @@ fbq('track', 'Purchase');`}</script>
         src={`//loox.io/widget/loox.js?shop=luciana-rose-couture.myshopify.com`}
       ></script>
       <script src="https://www.poptrkr.com/scripts/sdk/everflow.js" />
-      <script type="text/javascript">
-        {`
-            alert('ef?', window, EF)`}
-      </script>
+      <script>{`
+      if(EF !== 'undefined') if (!EF.getTransactionId(EF.urlParameter('oid')) || EF.urlParameter('_ef_transaction_id')) {
+    EF.click({
+        offer_id: EF.urlParameter('oid'),
+        affiliate_id: EF.urlParameter('affid'),
+        transaction_id: EF.urlParameter('_ef_transaction_id'),
+        sub1: EF.urlParameter('sub1'),
+        sub2: EF.urlParameter('sub2'),
+        sub3: EF.urlParameter('sub3'),
+        sub4: EF.urlParameter('sub4'),
+        sub5: EF.urlParameter('sub5'),
+        uid: EF.urlParameter('uid'),
+		source_id: EF.urlParameter('source_id'),
+		creative_id: EF.urlParameter('creative_id'),
+    }).then(function(transactionId) {
+        if (document.getElementById("cf_contact_affiliate_aff_sub")) {
+            document.getElementById('cf_contact_affiliate_aff_sub').value = transactionId
+            console.log('Everflow: filled aff_sub with transaction id: ' + transactionId)
+        } else {
+            console.log('Everflow: aff_sub element not found, relying on loop for transaction id: ' + transactionId)
+        }
+    });
+}
+var __checkExist = setInterval(function() {
+  if (document.getElementById("cf_contact_affiliate_aff_sub")) {
+    var currentAffsub = document.getElementById("cf_contact_affiliate_aff_sub").value;
+    var storedTransactionId = EF.getAdvertiserTransactionId(5);
+console.log(storedTransactionId, currentAffsub, storedTransactionId)
+    if(currentAffsub === 'undefined' || currentAffsub === '' && storedTransactionId) {
+        console.log('Everflow: updating aff_sub with persisted transaction id: ' + storedTransactionId)
+        clearInterval(__checkExist);
+        document.getElementById('cf_contact_affiliate_aff_sub').value = storedTransactionId
+    }
+  }
+}, 200);`}</script>
     </Helmet>
   );
 };
